41cbf7f61cf0ab9896d830688a8fe15e28951357
Leonardo Pereira <leopereirasilva86@gmail.com>
2023-03-31 16:35:30 -0300

    inicio implementacao tabuleiro

--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -15,5 +15,10 @@ namespace tabuleiro
             Colunas = colunas;
             Pecas = new Peca[Linhas, Colunas];
         }
+
+        public Peca peca(int linha, int coluna)
+        {
+            return Pecas[linha, coluna];
+        }
     }
 }
