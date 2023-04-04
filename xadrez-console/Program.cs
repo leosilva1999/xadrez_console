4a059d9bd609e9903959d2a6d5a427ac79d58b50
Leonardo <leopereirasilva86@gmail.com>
2023-04-03 21:20:05 -0300

    tratando erros com excessao personalizada

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -8,11 +8,18 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-            Tela.imprimirtabuleiro(tab);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
+
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                Tela.imprimirtabuleiro(tab);
+            }catch (TabuleiroException te) {
+                Console.WriteLine(te.Message);
+            }
+
             Console.ReadLine();
         }
     }
