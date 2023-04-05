105f188207d2951ce39fb1c2e4631cce2f6a7e07
Leonardo <leopereirasilva86@gmail.com>
2023-04-04 21:25:07 -0300

    cores as peças adicionadas

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -7,7 +7,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
     internal class Program
     {
         static void Main(string[] args)
-        {/**
+        {
             try
             {
                 Tabuleiro tab = new Tabuleiro(8, 8);
@@ -15,15 +15,11 @@ namespace MyApp // Note: actual namespace depends on the project name.
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
                 tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
                 Tela.imprimirtabuleiro(tab);
             }catch (TabuleiroException te) {
                 Console.WriteLine(te.Message);
-            }
-            **/
-
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
-            Console.WriteLine( pos);
-            Console.WriteLine(pos.toPosicao());
+            }                   
 
             Console.ReadLine();
         }
